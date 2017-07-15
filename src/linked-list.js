bc2671f46b42f09d23e5f7a7ff19cd3c211e4da3
Omar Zamarripa <omar.zamarripa@gmail.com>
2017-07-15 15:43:19 +0100

    Add linked list

---
+++ src/linked-list.js
@@ -0,0 +1,61 @@
+const assert = require("assert")
+const log = console.log.bind(this)
+
+class Node{
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push(value) {
+    const node = new Node(value)
+    if (this.head === null)
+      this.head = node
+    if (this.tail !== null) {
+      this.tail.next = node
+    }
+    this.tail = node
+  }
+
+  pop() {
+    let prevLastNode = this.head
+    while(prevLastNode && prevLastNode.next && prevLastNode.next.next) {
+      prevLastNode = prevLastNode.next
+    }
+    if(prevLastNode)
+      prevLastNode.next = null
+    this.tail = prevLastNode
+  }
+  get(index) {
+    let num = 0
+    let node = this.head
+    let value = null
+    while(node && num < index) {
+      num++
+      value = node.value
+      node = node.next
+    }
+
+    return (num === index) ? value : null
+  }
+}
+
+const list = new LinkedList()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+list.pop()
+
+log("head", list.head)
+log("tail", list.tail)
+log("get", list.get(5))
---
+++ src/quick-sort.js
@@ -0,0 +1,32 @@
+const assert = require("assert")
+const log = console.log.bind(this)
+
+function pivotSplit(list, pivot) {
+  // log(list, pivot)
+  const left = []
+  const right = []
+  while(list.length) {
+    if(list[0] < pivot) {
+      left.push(list.shift())
+    } else {
+      right.push(list.shift())
+    }
+  }
+
+  return [left, right]
+}
+
+function quickSort(list) {
+  if(list.length <= 1)
+    return list
+  const pivot = list.pop()
+  const [left, right] = pivotSplit(list, pivot)
+  // log(left, right)
+  return [].concat(quickSort(left), [pivot], quickSort(right))
+}
+
+const nums = [10,6,3,8,2,1,4,7,9,5]
+const sorted = [1,2,3,4,5,6,7,8,9,10]
+
+log(quickSort(nums.slice(0)))
+assert.deepEqual(quickSort(nums), sorted)
