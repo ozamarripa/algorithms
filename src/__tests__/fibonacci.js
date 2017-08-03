9b7ce2ca55147ba9adb530b8fce6076ac4025149
Omar Zamarripa <omar.zamarripa@gmail.com>
2017-08-03 07:09:27 +0100

    Add fibonacci

---
+++ src/__tests__/fibonacci.js
@@ -0,0 +1,54 @@
+const {fibonacci, recursiveFibonacci} = require("../fibonacci")
+const {expect} = require("chai")
+
+describe("Fibonacci", function() {
+
+
+  describe("Fibonacci", () => {
+
+    it("should return the 1 Fibonacci", () => {
+      expect(fibonacci(1)).to.equal(0)
+    })
+
+    it("should return the 2 Fibonacci", () => {
+      expect(fibonacci(2)).to.equal(1)
+    })
+
+    it("should return the 3 Fibonacci", () => {
+      expect(fibonacci(3)).to.equal(1)
+    })
+
+    it("should return the 4 Fibonacci", () => {
+      expect(fibonacci(4)).to.equal(2)
+    })
+
+    it("should return the 5 Fibonacci", () => {
+      expect(fibonacci(5)).to.equal(3)
+    })
+  })
+
+  describe("Recursive Fibonacci", () => {
+
+    it("should return the 1 Fibonacci", () => {
+      expect(recursiveFibonacci(1)).to.equal(0)
+    })
+
+    it("should return the 2 Fibonacci", () => {
+      expect(recursiveFibonacci(2)).to.equal(1)
+    })
+
+    it("should return the 3 Fibonacci", () => {
+      expect(recursiveFibonacci(3)).to.equal(1)
+    })
+
+    it("should return the 4 Fibonacci", () => {
+      expect(recursiveFibonacci(4)).to.equal(2)
+    })
+
+    it("should return the 5 Fibonacci", () => {
+      expect(recursiveFibonacci(5)).to.equal(3)
+    })
+
+  })
+
+})
---
+++ src/fibonacci.js
@@ -0,0 +1,35 @@
+
+log = console.log.bind(this)
+// 0 1 1 2 3 5 8 13
+
+// 3 = 2 + 1
+//
+// n = (n-1) + (n-2)
+
+function fibonacci(n) {
+
+  let first = 0
+  let second = 1
+  let newValue = first
+
+  for (let i = 1 ; i < n; i++) {
+    newValue = first + second
+    first = second
+    second = newValue
+  }
+
+  return first
+}
+
+function recursiveFibonacci(n) {
+  if (n <= 1)
+    return 0
+  if (n === 2)
+    return 1
+  return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+
+module.exports = {
+  fibonacci,
+  recursiveFibonacci
+}
